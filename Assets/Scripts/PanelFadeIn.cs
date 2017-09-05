5e1a23c5264d71dea58aab07ccdcbdf838a8592e
Scott Young <young.scottw@gmail.com>
2017-09-05 11:38:58 -0500

    Add start menu fade in

---
+++ Assets/Scripts/PanelFadeIn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelFadeIn : MonoBehaviour {
+	public float fadeInTime;
+	private Image fadePanel;
+
+	void Start () {
+		fadePanel = GameObject.Find("Fade Panel").GetComponent<Image>();
+		fadePanel.CrossFadeAlpha(0f, fadeInTime, false);
+	}
+}
