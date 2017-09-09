bad1fcb69c39592f072bccc4853c102a21824c0a
Scott Young <young.scottw@gmail.com>
2017-09-09 10:33:39 -0500

    Fix master volume bug on game restart

---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+	private MusicManager musicManager;
+
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		musicManager.ChangeVolume(PlayerPrefsManager.GetMasterVolume());
+	}
+}
