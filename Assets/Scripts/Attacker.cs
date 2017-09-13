a985a4c8b6e2d8acf5522da961e8a6514e268e11
Scott Young <young.scottw@gmail.com>
2017-09-13 16:39:00 -0500

    Create health script, attach to all prefabs, and deal with damage on defenders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class Attacker : MonoBehaviour {
 
 	private float walkSpeed;
 	private GameObject currentTarget;
-
+	private Animator anim;
 	// Use this for initialization
 	void Start () {
 		Rigidbody2D thisRigidbody = gameObject.AddComponent<Rigidbody2D>();
 		thisRigidbody.isKinematic = true;
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+		if(!currentTarget) {
+			anim.SetBool("isAttacking", false);
+		}
 	}
 
 	void OnTriggerEnter2D() {
@@ -28,7 +33,12 @@ public class Attacker : MonoBehaviour {
 
 	// Called from the animator at the time of attack
 	public void StrikeCurrentTarget(float damage) {
-		Debug.Log(name + " caused damage: " + damage);
+		if(currentTarget) {	
+			Health health = currentTarget.GetComponent<Health>();
+			if(health) {
+				health.DealDamage(damage);
+			}
+		}
 	}
 
 	public void Attack(GameObject obj) {
