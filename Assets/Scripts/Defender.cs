b231bcf7506fa6ed46b34ab4e2c6f45b54145b9b
Scott Young <young.scottw@gmail.com>
2017-09-14 16:44:50 -0500

    Add Shooter script for firing weapons and Shredder for destroying weapons that leave view, clean up other scripts

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -4,17 +4,4 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnTriggerEnter2D() {
-		Debug.Log(name + " trigger enter");
-	}
 }
